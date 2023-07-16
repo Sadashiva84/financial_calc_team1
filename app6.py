3ec733a2b274c787482258b0df4d30da305d2580
ram <24ram46r@gmail.com>
2023-07-16 12:06:59 +0530

    Feature 6 second commit

---
+++ app6.py
@@ -0,0 +1,11 @@
+# Feature 6
+savings = 0
+fin_health = 'Strong'
+print("Your Financial Health is classified as Strong/Normal/Weak ")
+if savings > 10000:
+    print("Your Financial Health is Strong, Well Done! ")
+elif savings > 5000 and savings < 7000:
+    print("Your Financial Health is Average, Can Be Better! ")
+else:
+    print("Your Financial Health is Weak, Please consult an advisor immediately! ")
+    
